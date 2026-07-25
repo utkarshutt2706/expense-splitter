c8742df966733fa700a88296f1b010c191b2006c
Utkarsh Srivastava <utkarshutt2706@gmail.com>
2026-07-25 15:54:30 +0530

    Chore: Add frances and ibm plex sans typography (#20)

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import '@fontsource-variable/fraunces';
+import '@fontsource-variable/ibm-plex-sans';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
